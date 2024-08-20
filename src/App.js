3fb72be72f80b95e8d803e3def8cffdde46daca0
ompeak <peakcary@163.com>
2024-08-20 19:04:45 +0800

    feat: react-responsive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "./App.css"; 
-import OllamaComponent from './components/OllamaComponent'
-import OllamaHComponent from './components/OllamaHComponent'
-import YuanqiComponent from './components/YuanqiComponent'
+import Example from './components/Example/index'
 
 function App() {  
   return (
     <div className="App"> 
-      {/* <OllamaComponent />
-      <OllamaHComponent />
-      <YuanqiComponent /> */}
-
-      <h1>Welcome to Retro Pixel World123456</h1>
-      <h2 className="h1-jockey">This is using Jockey One Font123456</h2>
+       <Example ></Example>
     </div>
   );
 }
