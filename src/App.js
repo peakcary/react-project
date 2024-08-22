f87d5cf6f959603186df486571086c7d24fc49bd
ompeak <peakcary@163.com>
2024-08-22 09:00:09 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import "./App.css"; 
-import OllamaComponent from './components/OllamaComponent'
-import OllamaHComponent from './components/OllamaHComponent'
-import YuanqiComponent from './components/YuanqiComponent'
+import "./App.css";  
 
 function App() {  
   return (
-    <div className="App"> 
-      {/* <OllamaComponent />
-      <OllamaHComponent />
-      <YuanqiComponent /> */}
+    <div className="App">  
 
       <h1>Welcome to Retro Pixel World123456</h1>
       <h2 className="h1-jockey">This is using Jockey One Font123456</h2>
